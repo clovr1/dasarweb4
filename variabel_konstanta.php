bcc07ee1cca2fb48f0b766899a0862c908203337
clovr1 <keenanaryasatya01@gmail.com>
2025-09-24 16:53:40 +0700

    Update variabel_konstanta.php

--- variabel_konstanta.php
+++ variabel_konstanta.php
@@ -1,26 +1,18 @@
 <?php
-$nama = "Budi Santoso";
-$umur = 21;
-echo "Halo, nama saya $nama dan usia saya $umur tahun.";
+$angka1 = 10;
+$angka2 = 5;
+$hasil = $angka1 + $angka2;
+echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil.";
 echo "<br>"; 
 
-$nilai = 95.5;
-$NILAI = 80; 
-echo "Nilai mata kuliah: $nilai. Nilai lain: $NILAI.";
-echo "<br>";
+$benar = true;
+$salah = false;
+echo "Variabel benar: $benar, Variabel salah: $salah";
+echo "<br>"; 
 
-define("GREETING", "Selamat datang di pemrograman PHP!");
-define("TAHUN_BERDIRI", 1995); 
+define("NAMA_SITUS", "WebsiteKu.com");
+define("TAHUN_PENDIRIAN", 2023);
 
-echo GREETING;
-echo "<br>";
-echo "PHP diciptakan pada tahun " . TAHUN_BERDIRI;
-echo "<br>";
-
-
-define("PI", 3.14159265);
-$radius = 7;
-$luas_lingkaran = PI * ($radius * $radius);
-echo "Luas lingkaran dengan jari-jari $radius adalah $luas_lingkaran.";
+echo "Selamat datang di " . NAMA_SITUS . ", situs yang didirikan pada tahun " . TAHUN_PENDIRIAN . ".";
 echo "<br>";
 ?>
